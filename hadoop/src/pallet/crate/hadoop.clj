b9d6eed742cee9667f88431a9c1ebac0cb068554
Sam Ritchie <sritchie09@gmail.com>
2011-04-01 01:19:11 -0400

    Updated documentation for funky new threading methods.

--- hadoop/src/pallet/crate/hadoop.clj
+++ hadoop/src/pallet/crate/hadoop.clj
@@ -142,84 +142,67 @@ INCOMPLETE - not yet ready for general use, but close!"
 ;; TODO -- Look at the second phasefn, here, and make sure that we
 ;; have a good way of ensuring that the first item is the argvec.
 
-;; This is Hugo Duncan's method from pallet 0.5.0... We include it here to
-;; support our new version of phase-fn, without explicitly upgrading
-;; everything.
-
 (defn check-session
   "Function that can check a session map to ensure it is a valid part of
-   phase definiton. It returns the session map.
+   phase definition. It returns the session map.
 
-   If this fails, then it is likely that you have an incorrect crate function,
-   which is failing to return its session map properly, or you have a non crate
-   function in the phase defintion."
-  ([session]
-     ;; we do not use a precondition in order to improve the error message
-     (when-not (and session (map? session))
-       (condition/raise
-        :type :invalid-session
-        :message
-        "Invalid session map in phase. Check for non crate functions,
-      improper crate functions, or problems in threading the session map
-      in your phase definition.
-
-      A crate function is a function that takes a session map and other
-      arguments, and returns a modified session map. Calls to crate functions
-      are often wrapped in a threading macro, -> or pallet.phase/phase-fn,
-      to simplify chaining of the session map argument."))
-     session)
-  ([session form]
-     ;; we do not use a precondition in order to improve the error message
-     (when-not (and session (map? session))
-       (condition/raise
-        :type :invalid-session
-        :message
-        (format
-         (str
-          "Invalid session map in phase session.\n"
-          "`session` is %s\n"
-          "Problem probably caused in:\n  %s ")
-         session form)))
-     session))
-
-;; Here's an older version of phase-fn.
-(defmacro phase-fn
-  "Composes a phase function from a sequence of phases by threading an
- implicit phase session parameter through each. Each phase will have
- access to the parameters passed in through `phase-fn`'s argument
- vector. thus,
-
-    (phase-fn [filename]
-         (file filename)
-         (file \"/other-file\"))
-
-   is equivalent to:
-
-   (fn [session filename]
-     (-> session
-         (file filename)
-         (file \"/other-file\")))
-  
-   with an added safety call to `check-session` prior to each phase
-   invocation."
-  ([argvec] `(phase-fn ~argvec identity))
-  ([argvec subphase & left]
-     `(fn [session# ~@argvec]
-        (--> session#
-             ~subphase
-             (check-session (str "The session passed out of " '~subphase))
-             ~@(when left
-                 [`((phase-fn ~argvec ~@left) ~@argvec)])))))
+   On failure, the function will print the phase through which the
+   session passed prior to crashing. It is like that this phase
+   introduced the fault into the session; to aid in debugging, make
+   sure to use `phase-fn` and `def-phase-fn` to build phases."
+  [session form]
+  ;; we do not use a precondition in order to improve the error message
+  (when-not (and session (map? session))
+    (condition/raise
+     :type :invalid-session
+     :message
+     (str
+      "Invalid session map in phase.\n"
+      (format "session is %s\n" session)
+      (format "Problem probably caused by subphase:\n  %s\n" form)
+      "Check for non crate functions, improper crate functions, or
+      problems in threading the session map in your phase
+      definition. A crate function is a function that takes a session
+      map and other arguments, and returns a modified session
+      map. Calls to crate functions are often wrapped in a threading
+      macro, -> or pallet.phase/phase-fn, to simplify chaining of the
+      session map argument.")))
+  session)
 
 ;; To qualify as a checker, a function must take two arguments -- the
 ;; threaded argument, and a description of the form from which it's
 ;; just returned.
 
 ;; TODO -- have this thing do arglists properly.
-(defmacro defthreader
-  "Produces functions like `phase-fn` above, with the option for
-  custom checkers inserted after every threading call. `phase-fn`
-  becomes a special case."
+(defmacro defthreadfn
+  "Binds a var to a particular class of anonymous functions that
+  accept a vector of arguments and a number of subexpressions. When
+  calling the produced functions, the caller must supply a threading
+  parameter as the first argument. This parameter will be threaded
+  through the resulting forms; the extra parameters made available in
+  the argument vector will be available to all subexpressions.
+
+  In addition to the argument vector, `defthreadfn` accepts any number
+  of watchdog functions, each of which are inserted into the thread
+  after each subexpression invocation. These watchdog functions are
+  required to accept 2 arguments -- the threaded argument, and a
+  string representation of the previous subexpression. For example,
+
+    (defthreadfn mapped-thread-fn
+      (fn [arg sub-expr]
+        (if (map? arg)
+          arg
+          (condition/raise
+           :type :invalid-session
+           :message
+           (format \"Only maps are allowed, and %s is most definitely
+                   not a map.\" sub-expr)))))
+
+  Returns a `thread-fn` that makes sure the threaded argument remains
+  a map, on every step of its journey.
+
+  For a more specific example if `defthreadfn`, see
+  `pallet.phase/phase-fn`."
   [macro-name & rest]
   (let [[macro-name checkers] (name-with-attributes macro-name rest)]
     `(defmacro ~macro-name
@@ -233,7 +216,7 @@ INCOMPLETE - not yet ready for general use, but close!"
                   ~@(when left#
                       [`((~'~macro-name ~argvec# ~@left#) ~@argvec#)])))))))
 
-(defthreader phase-fn
+(defthreadfn phase-fn
   "Composes a phase function from a sequence of phases by threading an
  implicit phase session parameter through each. Each phase will have
  access to the parameters passed in through `phase-fn`'s argument
@@ -250,11 +233,11 @@ INCOMPLETE - not yet ready for general use, but close!"
          (file filename)
          (file \"/other-file\")))
   
-   with an added safety call to `check-session` prior to each phase
-   invocation."
+   with a number of verifications on the session map performed after
+   each phase invocation."
   check-session)
 
-(defthreader unchecked-phase-fn
+(defthreadfn unchecked-phase-fn
   "Unchecked version of `phase-fn`.
 
    The following two forms are equivalent:
@@ -266,7 +249,7 @@ INCOMPLETE - not yet ready for general use, but close!"
    (fn [session x y]
        (-> session
            (+ x)
-           (+ y)))")
+           (+ y)))") 
 
 ;; TODO -- Support for various arg lists?
 (defmacro defphase
